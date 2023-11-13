f69b3dbed2a2fe155976004f7153815b8c94133d
egogit <egovici@naver.com>
2023-11-14 02:00:49 +0900

    feat: apply all info into CV site
    finish first sprint for base CV site

--- src/components/CVContent.js
+++ src/components/CVContent.js
@@ -6,12 +6,18 @@ import name from "../assets/name.png";
 const styles={
     cvContainer:{
         display:'flex',
+        paddingBottom: '20px',
     },
     cvTitle:{
         color: '#003b6c',
+        borderTop: '2px solid #003b6c',
+        borderBottom: '2px solid #003b6c'
     },
     termContainer:{
         width: 100,
+    },
+    cvElementContainer:{
+        flexDirection: 'column'
     },
 }
 
@@ -22,6 +28,7 @@ function CVContent(props){
     const [userEducation, setUserEducation] = useState([]);
     const [userCertificate, setUserCertificate] = useState([]);
     const [userProject, setUserProject] = useState([]);
+    const [userExtraActivity, setUserExtraActivity] = useState([]);
 
     const baseURL ="http://localhost:8080/api/user"
 
@@ -35,7 +42,12 @@ function CVContent(props){
 
     useEffect(() => {
         axios.get(baseURL+"/experience").then((res) => {
-            setUserExp([res.data[0]['name']]);
+            setUserExp([]);
+            res.data.map((exp)=>{
+                setUserExp( (userExp) =>
+                    [...userExp, [exp["id"], exp["type"], exp["name"],
+                        exp["location"], exp["term"],exp['content']]])
+            })
         }).catch((err) =>{
             console.log(err);
         })
@@ -43,7 +55,13 @@ function CVContent(props){
 
     useEffect(() => {
         axios.get(baseURL+"/education").then((res) => {
-            setUserEducation([res.data[0]['major']]);
+            setUserEducation([]);
+            res.data.map((exp)=>{
+                setUserEducation( (userEducation) =>
+                    [...userEducation, [exp["id"], exp["major"], exp["type"],
+                        exp["degree"], exp["school"],exp['GPA'],exp['relatedSubject']
+                    , exp['term']]])
+            })
         }).catch((err) =>{
             console.log(err);
         })
@@ -51,7 +69,12 @@ function CVContent(props){
 
     useEffect(() => {
         axios.get(baseURL+"/certificate").then((res) => {
-            setUserCertificate([res.data[0]['name']]);
+            setUserCertificate([]);
+            res.data.map((exp)=>{
+                setUserCertificate( (userCertificate) =>
+                    [...userCertificate, [exp["id"], exp["name"], exp["organization"], exp["date"]]]
+                )
+            })
         }).catch((err) =>{
             console.log(err);
         })
@@ -59,7 +82,27 @@ function CVContent(props){
 
     useEffect(() => {
         axios.get(baseURL+"/project").then((res) => {
-            setUserProject([res.data[0]['name']]);
+            setUserProject([]);
+            res.data.map((project)=>{
+                setUserProject( (userProject) =>
+                    [...userProject, [project["id"], project["name"], project["stack"], project["content"],
+                    project["contribution"],project["term"]]]
+                )
+            })
+        }).catch((err) =>{
+            console.log(err);
+        })
+    },[])
+
+    useEffect(() => {
+        axios.get(baseURL+"/extracurriculum").then((res) => {
+            setUserExtraActivity([]);
+            res.data.map((activity)=>{
+                setUserExtraActivity( (userExtraActivity) =>
+                    [...userExtraActivity, [activity["id"], activity["name"],
+                        activity["content"], activity["term"]]]
+                )
+            })
         }).catch((err) =>{
             console.log(err);
         })
@@ -74,39 +117,118 @@ function CVContent(props){
                 {userInfo[0]}
             </div>
             <div>
-                <h3 style={styles.cvTitle}>● Experience</h3>
-                <div style={styles.cvContainer}>
-                    <div style={styles.termContainer}>기간</div>
-                    <div>
-                        {userExp[0]}
-                    </div>
+                <div>
+                    <h3 style={styles.cvTitle}>Experience</h3>
+                </div>
+                <div style={styles.cvElementContainer}>
+                    {
+
+                        userExp.map((exp) => {
+                            return(
+                                <div key={exp[0]} style={styles.cvContainer}>
+                                    <div style={styles.termContainer}>
+                                        {exp[4]}
+                                    </div>
+                                    <div>
+                                        <div><b>{exp[3]} ({exp[2]}, {exp[1]})</b></div>
+                                        {exp[5]}
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
             <div>
-                <h3 style={styles.cvTitle}>● Education</h3>
-                <div style={styles.cvContainer}>
-                    <div style={styles.termContainer}>기간</div>
-                    <div>
-                        {userEducation[0]}
-                    </div>
+                <div>
+                    <h3 style={styles.cvTitle}>Education</h3>
+                </div>
+                <div style={styles.cvElementContainer}>
+                    {
+                        userEducation.map((education) => {
+                            return(
+                                <div key={education[0]} style={styles.cvContainer}>
+                                    <div style={styles.termContainer}>
+                                        {education[7]}
+                                    </div>
+                                    <div>
+                                        <div><b>{education[1]}, {education[3]}</b> </div>
+                                        <div><i>{education[4]}</i></div>
+                                        <div><b>GPA: </b>{education[5]}</div>
+                                        <div><b>Related coursework:</b>{education[6]}</div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
             <div>
-                <h3 style={styles.cvTitle}>● Certificate</h3>
-                <div style={styles.cvContainer}>
-                    <div style={styles.termContainer}>기간</div>
-                    <div>
-                        {userCertificate[0]}
-                    </div>
+                <div>
+                    <h3 style={styles.cvTitle}>Certificate</h3>
+                </div>
+                <div style={styles.cvElementContainer}>
+                    {
+                        userCertificate.map((certificate) => {
+                            return(
+                                <div key={certificate[0]} style={styles.cvContainer}>
+                                    <div style={styles.termContainer}>
+                                        {certificate[3]}
+                                    </div>
+                                    <div>
+                                        <div><b>{certificate[1]}</b></div>
+                                        <div>{certificate[2]}</div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
             <div>
-                <h3 style={styles.cvTitle}>● Projects</h3>
-                <div style={styles.cvContainer}>
-                    <div style={styles.termContainer}>기간</div>
-                    <div>
-                        {userProject[0]}
-                    </div>
+                <div>
+                    <h3 style={styles.cvTitle}>Projects</h3>
+                </div>
+                <div style={styles.cvElementContainer}>
+                    {
+                        userProject.map((project) => {
+                            return(
+                                <div key={project[0]} style={styles.cvContainer}>
+                                    <div style={styles.termContainer}>
+                                        {project[5]}
+                                    </div>
+                                    <div>
+                                        <div><b>{project[1]}</b></div>
+                                        <div><b>Stack:</b> {project[2]}</div>
+                                        <div><b>Content:</b> {project[3]}</div>
+                                        <div><b>Contribution:</b> {project[4]}%</div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h3 style={styles.cvTitle}>Extra Activity</h3>
+                </div>
+                <div style={styles.cvElementContainer}>
+                    {
+                        userExtraActivity.map((activity) => {
+                            return(
+                                <div key={activity[0]} style={styles.cvContainer}>
+                                    <div style={styles.termContainer}>
+                                        {activity[3]}
+                                    </div>
+                                    <div>
+                                        <div><b>{activity[1]}</b></div>
+                                        <div>{activity[2]}</div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </div>
--- src/components/CVSidebar.js
+++ src/components/CVSidebar.js
@@ -1,8 +1,9 @@
 import email from '../assets/email.png';
-import phone from '../assets/phone.png';
 import repo from '../assets/github-mark.png'
 import blog from '../assets/blog.png';
 import axios from 'axios';
+import StarRatings from "react-star-ratings/build/star-ratings";
+
 import {useState, useEffect} from "react";
 
 const styles = {
@@ -17,10 +18,21 @@ const styles = {
         width: 40,
         height: 40,
     },
-    contactImageContainer:{
-        display:'flex',
-        alignContent:'center',
-    }
+    contactImageContainer: {
+        display: 'flex',
+        alignContent: 'center',
+    },
+    infoUrl: {
+        textDecoration: 'none',
+        color: 'black',
+    },
+    skillContainer:{
+        display: 'flex',
+    },
+    skillRating:{
+        textAlign: 'right',
+        width: '100%'
+    },
 }
 
 function CVSidebar(props){
@@ -33,7 +45,7 @@ function CVSidebar(props){
 
     useEffect(() => {
         axios.get(baseURL+"/info").then((res) => {
-            setUserInfo([res.data[0]["email"],res.data[0]["phone"],res.data[0]["blog"],res.data[0]["git"]]);
+            setUserInfo([res.data[0]["email"],res.data[0]["blog"],res.data[0]["git"]]);
         }).catch((err) =>{
             console.log(err);
         })
@@ -41,7 +53,10 @@ function CVSidebar(props){
 
     useEffect(() => {
         axios.get(baseURL+"/skill").then((res) => {
-            setUserSkill([res.data[0]["name"],res.data[0]["degree"]]);
+            setUserSkill([]);
+            res.data.map((skill)=>{
+                setUserSkill( (userSkill) => [...userSkill, [skill["id"], skill["name"], skill["degree"]]])
+            })
         }).catch((err) =>{
             console.log(err);
         })
@@ -49,7 +64,11 @@ function CVSidebar(props){
 
     useEffect(() => {
         axios.get(baseURL+"/interests").then((res) => {
-            setUserInterests([res.data[0]["name"]]);
+            setUserInterests([]);
+            res.data.map((interests)=>{
+                setUserInterests( (userInterests) =>
+                    [...userInterests, [interests["id"], interests["name"]]])
+            })
         }).catch((err) =>{
             console.log(err);
         })
@@ -58,32 +77,44 @@ function CVSidebar(props){
     return(
         <div style={styles.sidebarContainer}>
             <div>
-                <h3 style={styles.cvTitle}>● Personal Info</h3>
+                <h3 style={styles.cvTitle}>Personal Info</h3>
                 <div>
                     <div style={styles.contactImageContainer}>
-                        <img src={email} style={styles.contactImage} alt="email"/><a href={`mailto:${userInfo[0]}`}>email</a>
+                        <img src={email} style={styles.contactImage} alt="email"/>
+                        <a href={`mailto:${userInfo[0]}` } style={styles.infoUrl}>Email</a>
                     </div>
                     <div style={styles.contactImageContainer}>
-                        <img src={phone} style={styles.contactImage} alt="phone"/>{userInfo[1]}
+                        <img src={repo} style={styles.contactImage} alt="git"/>
+                        <a href={userInfo[2]} style={styles.infoUrl}>Github</a>
                     </div>
                     <div style={styles.contactImageContainer}>
-                        <img src={repo} style={styles.contactImage} alt="git"/><a>{userInfo[2]}</a>
-                    </div>
-                    <div style={styles.contactImageContainer}>
-                        <img src={blog} style={styles.contactImage} alt="blog"/><a>{userInfo[3]}</a>
+                        <img src={blog} style={styles.contactImage} alt="blog"/>
+                        <a href={userInfo[1]} style={styles.infoUrl}>Min It's Devlog</a>
                     </div>
                 </div>
             </div>
             <div>
-                <h3 style={styles.cvTitle}>● Skills</h3>
+                <h3 style={styles.cvTitle}>Skills</h3>
                 <div>
-                    {userSkill[0]}
+                    {
+                        userSkill.map((skill) => {
+                            return <div style={styles.skillContainer}>
+                                <div key={skill[0]}>{skill[1]}</div>
+                                <div style={styles.skillRating}>
+                                    <StarRatings rating={skill[2]} starRatedColor="#003b6c"
+                                                 starSpacing='3px' starDimension="15px"/>
+                                </div>
+                            </div>
+                    })}
                 </div>
             </div>
             <div>
-                <h3 style={styles.cvTitle}>● Interests</h3>
+                <h3 style={styles.cvTitle}>Interests</h3>
                 <div>
-                    {userInterests[0]}
+                    {
+                        userInterests.map((interests) => {
+                            return <div key={interests[0]}>{interests[1]}</div>
+                        })}
                 </div>
             </div>
         </div>
